1206befaccd6b4b2a9c8f979382783a5a88543a7
D J Capelis <dev@capelis.dj>
2012-05-30 04:14:00 -0700

    Drop flags for stack and heap
    I'll implement these later if I really need them.

--- region_list.h
+++ region_list.h
@@ -3,16 +3,12 @@
 
 #include "djc_defines.h"
 
-#define RL_FLAG_STACK       (1 << 0) /* bit 0 */ /* Mem region is [stack] */
-#define RL_FLAG_HEAP        (1 << 1) /* bit 1 */ /* Mem region is [heap] */
-
 #define RL_FLAG_RWANON      (1 << 0) /* bit 0 */ /* Only grab rw anonymous mappings */
 
 struct region_list
 {
     void * begin;
     void * end;
-    int flags; /* Per defines, above */
     struct region_list * next;
 };
 
